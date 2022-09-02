f159d08cd6fad1dfd2bfec68f9261823a10913f2
larkz <liu.larkin@gmail.com>
2022-09-02 16:42:53 +0200

    added node code

--- mcts4py/NodeClasses.py
+++ mcts4py/NodeClasses.py
@@ -1,6 +1,8 @@
 from enum import Enum
-from Node import *
+# from Node import *
 import numpy as np
+from abc import ABC, abstractmethod
+
 
 # Sample Enum definition,
 # class Event(Enum):
@@ -14,10 +16,10 @@ class Node():
     
     def __init__(self, 
         parent = None,
-        inducingAction = None):
+        inducing_action = None):
 
         self.parent = parent
-        self.inducingAction = inducingAction
+        self.inducing_action = inducing_action
 
         self.depth = 0 if parent == None else parent.depth + 1
 
@@ -25,46 +27,79 @@ class Node():
         self.reward = 0.0
         self.max_reward = 0.0
 
+    @abstractmethod
     def addChild(self):
+        # pass
         raise NotImplementedError("Please Implement this method")
 
+    @abstractmethod
     def getChildren(self):
+        # pass
         raise NotImplementedError("Please Implement this method")
-
 
 # Stateful action node
 class ActionNode(Node):
-
     def __init__(self, 
         value,
-        parentState = None,
+        parent = None,
         children = []):
 
         self.value = value
-        self.parentState = parentState
+        self.parent = parent
         self.children = children
 
-        def addChild(self, child):
-            self.children.append(child)
+    def addChild(self, child):
+        self.children.append(child)
 
-        def getChildren(self, state):
-            return filter(lambda x: x.parentState == state, self.children)
+    def getChildren(self, action):
+        if action == None:
+            return self.children
+        else:
+            return filter(lambda x: x.inducing_action == action, self.children) 
+        # return filter(lambda x: x.parentState == state, self.children)
+    
+    def __str__(self):
+        return 'Action: {}, Max Reward: {}'.format(str(self.inducing_action), str(self.max_reward)) 
 
 # Stateful state node
-class StateNode(Node):
+# class StateNode(Node):
 
     def __init__(self, 
         value,
-        inducingAction = None,
-        valid_actions = []):
+        state,
+        inducing_action = None,
+        valid_actions = [],
+        is_terminal = False):
 
         self.value = value
-        self.inducingAction = inducingAction
+        self.inducing_action = inducing_action
         self.children = valid_actions
+        self.state = state
 
-        def addChild(self, child):
-            self.children.append(child)
+    def addChild(self, child):
+        if child.inducing_action == None:
+            raise Exception("Inducing action must be set on child")
+        if child.inducing_action in self.children.keys():
+            raise Exception("A child with the same inducing action has already been added")
+        self.children[repr(child.inducing_action)] = child
 
-        def getChildren(self, action):
-            return filter(lambda x: x.inducingAction == action, self.children)
+    def getChildren(self, action):
+        if action == None:
+            return self.children.values()
+        else:
+            child = self.children[action]
+            if child == None:
+                return []
+            else:
+                return [child]
+    
+    def exploredActions(self):
+        return self.children.keys()
+    
+    def __str__(self):
+        return 'State: {}, Max Reward: {}'.format(str(self.state), str(self.max_reward)) 
+    
+    # fun exploredActions(): Collection<ActionType> {
+    #     return children.keys
+    # }
 
